98a90265b790e11c5ad275bd30929a09bf49bd7d
GiseleBCSantos <giselebianca57@gmail.com>
2025-08-31 20:39:06 -0300

    :sparkles: Update ListScene component to wrap Card elements in Link for navigation

--- frontend/src/pages/scene/list-scene/index.tsx
+++ frontend/src/pages/scene/list-scene/index.tsx
@@ -1,24 +1,25 @@
+import { Link } from "react-router";
 import { ActionButton } from "../../../components/action-button";
 import { Card } from "../../../components/card";
 
 export const ListScene = () => {
   return (
     <div className="relative p-5 pb-[150px] ">
-      <div className="absolute right-3 top-3 w-[50px] h-[50px]">
-        <ActionButton icon="plus" />
-      </div>
+      <ActionButton icon="plus" />
       <h1 className="text-center text-2xl font-bold">Cenas</h1>
 
       <div className="grid grid-cols-2 pb-30 gap-5 mt-5">
         {scenesMock.map((scene) => (
-          <Card
-            key={scene.id}
-            title={scene.name}
-            showStatus={true}
-            status={scene.state}
-            icon={scene.icon}
-            toggleStatus={scene.in_progress}
-          />
+          <Link to={`/cenas/${scene.id}`} key={scene.id} state={scene}>
+            <Card
+              key={scene.id}
+              title={scene.name}
+              showStatus={true}
+              status={scene.state}
+              icon={scene.icon}
+              toggleStatus={scene.in_progress}
+            />
+          </Link>
         ))}
       </div>
     </div>
