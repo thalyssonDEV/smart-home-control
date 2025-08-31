0881c8a193f81c9f2570efa6c027aca6035bed00
GiseleBCSantos <giselebianca57@gmail.com>
2025-08-31 20:30:37 -0300

    :recycle: Add submit and onClick props

--- frontend/src/components/action-button/index.tsx
+++ frontend/src/components/action-button/index.tsx
@@ -1,13 +1,17 @@
 interface ActionButtonProps {
   icon: string;
+  submit?: boolean;
+  onClick?: () => void;
 }
 
-export const ActionButton = ({ icon }: ActionButtonProps) => {
+export const ActionButton = ({ icon, submit, onClick }: ActionButtonProps) => {
   return (
-    <div className="shadow-md bg-secondary justify-center items-center flex rounded-md w-full h-full">
-      <button>
-        <i className={`fa-solid fa-${icon}`} />
-      </button>
+    <div className="absolute right-2 top-2 w-[50px] h-[50px]">
+      <div className="shadow-md bg-secondary justify-center items-center flex rounded-md w-full h-full">
+        <button type={submit ? "submit" : "button"} onClick={onClick}>
+          <i className={`fa-solid fa-${icon}`} />
+        </button>
+      </div>
     </div>
   );
 };
