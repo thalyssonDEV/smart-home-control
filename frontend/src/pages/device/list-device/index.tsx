bf3a397e488329828fafc2abde7dcfd27740ea2e
GiseleBCSantos <giselebianca57@gmail.com>
2025-08-31 20:40:01 -0300

    :sparkles: Refactor ListDevice component to accept devices as props

--- frontend/src/pages/device/list-device/index.tsx
+++ frontend/src/pages/device/list-device/index.tsx
@@ -1,17 +1,29 @@
+import { Link } from "react-router-dom";
 import { ActionButton } from "../../../components/action-button";
 import { Card } from "../../../components/card";
 
-export const ListDevice = () => {
+interface ListDeviceProps {
+  devices?: Array<{
+    id: number;
+    name: string;
+    icon: string;
+    state: boolean;
+    room: number;
+  }>;
+}
+
+export const ListDevice = ({ devices = devicesMock }: ListDeviceProps) => {
   return (
     <div className="relative p-5 pb-[150px] ">
-      <div className="absolute right-3 top-3 w-[50px] h-[50px]">
+      <Link to={`/dispositivos/novo`}>
         <ActionButton icon="plus" />
-      </div>
+      </Link>
       <h1 className="text-center text-2xl font-bold">Dispositivos</h1>
 
       <div className="grid grid-cols-2 pb-30 gap-5 mt-5">
-        {devicesMock.map((device) => (
+        {devices.map((device) => (
           <Card
+            key={device.id}
             title={device.name}
             icon={device.icon}
             showStatus={true}
